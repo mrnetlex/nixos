37582e17ce3106005327668b5924abf726088e8b
netlex <mrnetlex@gmail.com>
2024-03-09 03:34:30 +0100

    further improvements for best image quality and code readability

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -4,71 +4,51 @@
 }: {
   programs.mpv = {
     enable = true;
-    profiles = {
-      SDR = {
-        profile = "gpu-hq";
-        vo = "gpu-next";
-        target-colorspace-hint = true;
-        gpu-api = "vulkan";
-        gpu-context = "waylandvk";
-        scale = "ewa_lanczossharp";
-        cscale = "ewa_lanczossharp";
-        tscale = "oversample";
-        video-sync = "display-resample";
-        interpolation = true;
-        audio-channels = "stereo";
-      };
-      HDR = {
-        profile = "gpu-hq";
-        vo = "gpu-next";
-        target-colorspace-hint = true;
-        gpu-api = "vulkan";
-        gpu-context = "waylandvk";
-        video-output-levels = "limited";
-        scale = "ewa_lanczossharp";
-        cscale = "ewa_lanczossharp";
-        tscale = "oversample";
-        video-sync = "display-resample";
-        interpolation = true;
-        gamma = 8;
-        brightness = 1;
-        audio-channels = "stereo";
-      };
-      test = {
-      	fullscreen = true;
+    config = {
+		fullscreen = true;
       	fs-screen = 0;
         screen = 0;
       	window-maximized = "yes";
       	keep-open = "no";
         alang = "eng,en,enUS,en-US";
-      	
-      	vo = "gpu-next";
-      	gpu-api = "vulkan";
-      	gpu-context = "waylandvk";
-      	target-colorspace-hint = true;
-      	
-      	profile = "gpu-hq";
-      	hwdec = "auto-safe";
-      	
-      	ytdl-format = "bv*[height<=2160]+ba/b[height<=2160]";
-      	scale = "ewa_lanczossharp";
-      	cscale = "ewa_lanczossharp";
-      	deband = true;
-      	      
-      	interpolation = true;
-      	video-sync = "display-resample";
-      	tscale = "oversample";
+        slang = "eng,en,enUS,en-US";
+        
+        profile = "gpu-hq";
+        vo = "gpu-next";
+        hwdec = "auto-safe";
+        target-colorspace-hint = true;
+        gpu-api = "vulkan";
+        gpu-context = "waylandvk";
+        
+        scale = "ewa_lanczossharp";
+        dscale= "mitchell";
+        cscale = "ewa_lanczossharp";
+        tscale = "oversample";
+        video-sync = "display-resample";
+        interpolation = true;
+        deband = true;
+        dither-depth = "auto";
+        
+        audio-channels = "stereo";
+    };
+    profiles = {
+      SDR = {
+		video-output-levels = "full";
+      };
+      HDR = {
+        video-output-levels = "limited";
+        gamma = 8;
+        brightness = 2;
       };
       clean = {
       	
       };
     };
-    defaultProfiles = [ "test" ];
+    defaultProfiles = [ "HDR" ];
     bindings = {
       "Ctrl+1" = "apply-profile SDR";
       "Ctrl+2" = "apply-profile HDR";
-      "Ctrl+3" = "apply-profile test";
-      "Ctrl+4" = "apply-profile clean";
+      "Ctrl+3" = "apply-profile clean";
     };
   };
 }
