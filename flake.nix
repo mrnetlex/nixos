5361314fcec440eb3648eb1c45ed565eb94e40b6
netlex <mrnetlex@gmail.com>
2024-07-18 02:44:09 +0200

    merge both nixos repos

--- flake.nix
+++ flake.nix
@@ -3,20 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    NixOS-WSL.url = "github:nix-community/NixOS-WSL";
+    NixOS-WSL.inputs.nixpkgs.follows = "nixpkgs";
+    
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-flatpak.url = "github:gmodena/nix-flatpak";
+
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
     stylix.url = "github:danth/stylix";
+
     spicetify-nix.url = "github:the-argus/spicetify-nix";
   };
 
   outputs =
-    inputs @ { nixpkgs
-    , home-manager
-    , ...
-    }: {
+    inputs @ { self, nixpkgs, NixOS-WSL, home-manager, ...}: {
       nixosConfigurations = {
         workstation = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
@@ -29,7 +34,23 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.netlex = import ./home.nix;
+              home-manager.users.netlex = import ./hosts/workstation/home.nix;
+            }
+          ];
+          specialArgs = { inherit inputs; };
+        };
+
+        ASUS_B560_WSL = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { nix.registry.nixpkgs.flake = nixpkgs; }
+            ./hosts/WSL/configuration.nix
+            NixOS-WSL.nixosModules.wsl
+            home-manager.nixosModules.home-manager
+            {
+        	  home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.netlex = import ./host/WSL/home.nix;
             }
           ];
           specialArgs = { inherit inputs; };
