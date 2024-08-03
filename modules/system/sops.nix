34712db4da43dbe290a247b319706990eedca15a
netlex <mrnetlex@gmail.com>
2024-08-04 01:58:31 +0200

    changed location of keys.txt
    I needed to change location of keys.txt - my master key that unlocks
    secret.yaml. I don't know why but for some reason it wouldnt let my use
    `sops secrets.yaml` and complain that it cant find the file, but during
    system rebuild everything would work fine. Eiher way I changed location
    and my documentation and everything is fine.

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -16,7 +16,7 @@
       # automatically impost host SSH keys as age keys
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       # this will use an age key that is expected to already be in filesystem
-      keyFile = "/home/keys.txt";
+      keyFile = "/home/netlex/.config/sops/age/keys.txt";
       # generate a new key if the key specified above does not exist
       generateKey = true;
     };
