871ea9817778a0b3b35e6c863c419b1dfde878ed
netlex <mrnetlex@gmail.com>
2024-08-04 02:18:54 +0200

    added pool as samba share, removed smb-secrets in plain text

--- modules/system/storage/samba.nix
+++ modules/system/storage/samba.nix
@@ -26,4 +26,15 @@
       in
       [ "${automount_opts},credentials=${config.sops.secrets.smb-creds.path},uid=1000,gid=100" ];
   };
+  #pool
+  fileSystems."/mnt/pool" = {
+    device = "//192.168.1.148/pool";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in
+      [ "${automount_opts},credentials=${config.sops.secrets.smb-creds.path},uid=1000,gid=100" ];
+  };
 }
