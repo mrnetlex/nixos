a287b53af913ded78d0e1848184ff22153deaf8a
netlex <mrnetlex@gmail.com>
2024-08-05 00:32:49 +0200

    more firefox options

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,15 +1,29 @@
 {config, pkgs, ...}:
 
 {
-    programs = {
-    firefox = {
+    programs.firefox = {
       enable = true;
       languagePacks = [ "pl" "en-GB" ];
       policies = {
-              DisableTelemetry = true;
-              DisableFirefoxStudies = true;
-              DisablePocket = true;
-              DisableFirefoxScreenshots = true;
-              DontCheckDefaultBrowser = true;
-             };
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableFirefoxScreenshots = true;
+        DontCheckDefaultBrowser = true;
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+      # settings = {
+      #   "browser.aboutConfig.showWarning" = false;
+      #   "browser.tabs.warnOnCloseOtherTabs" = false;
+      # };
+   	};
 }
